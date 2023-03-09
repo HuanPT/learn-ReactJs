233f0ac5a565cd5d4c1b77a9c344d146a2ec97b9
HuanPT <lexus.hc293@gmail.com>
2023-03-09 20:31:19 +0700

    re update

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    assetsDir: "./",
-    emptyOutDir: true,
-    manifest: true,
-  },
-});
+})
+
+// export default defineConfig({
+//   base: "./index.html",
+//   plugins: [react()],
+//   build: {
+//     outDir: "dist",
+//     assetsDir: "./",
+//     emptyOutDir: true,
+//     manifest: true,
+//   },
+// });
