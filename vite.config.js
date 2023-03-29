20d720897f4473ca4b6a80a180830aaa7e5468d1
HuanPT <lexus.hc293@gmail.com>
2023-03-29 21:09:11 +0700

    Huan fix dist 29/3

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
 // export default defineConfig({
-//   base: "./index.html",
 //   plugins: [react()],
-//   build: {
-//     outDir: "dist",
-//     assetsDir: "./",
-//     emptyOutDir: true,
-//     manifest: true,
-//   },
-// });
+// })
+
+export default defineConfig({
+  base: "",
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "./",
+    emptyOutDir: true,
+    manifest: true,
+  },
+});
+
